6bb57faab0e48ba5855ad836ef69e0e3a68608d7
RochaSzl <132150468+RochaSzl@users.noreply.github.com>
2024-11-25 10:56:36 -0300

    Delete SupabaseContext.tsx

--- SupabaseContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://olxfvicqflitnfwukmjr.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9seGZ2aWNxZmxpdG5md3VrbWpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA5NDgwMDQsImV4cCI6MjA0NjUyNDAwNH0.YxQRe9KX_npsPGMQJXeO3QwNtFUD_lKhtqvFzDQvm6w';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
